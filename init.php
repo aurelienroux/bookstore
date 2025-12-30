3aa1530d11d9a28c10656b7ee3609d0934acfd6a
Aurelien Roux <aureroux13@gmail.com>
2025-12-30 19:17:25 +0100

    classes autoloader

--- init.php
+++ init.php
@@ -3,11 +3,12 @@
 use Bookstore\Domain\Book;
 use Bookstore\Domain\Customer;
 
-require_once __DIR__ . '/Book.php';
-require_once __DIR__ . '/Customer.php';
-
-$book1 = new Book(9877633, "1984", "George Orwell", 12);
-$book2 = new Book(7783934, "To kill", "Harper Lee", 2);
-
-$customer1 = new Customer(1, "John", "Doe", "john.doe@gmail.com");
-$customer2 = new Customer(2, "Mary", "Poppins", "mary.poppins@gmail.com");
+function autoloader($classname)
+{
+  $lastSlash = strpos($classname, '\\') + 1;
+  $classname = substr($classname, $lastSlash);
+  $directory = str_replace('\\', '/', $classname);
+  $filename = __DIR__ . '/src/' . $directory . '.php';
+  require_once($filename);
+}
+spl_autoload_register('autoloader');
